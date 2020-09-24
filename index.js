c147097e14975802f96ff6119774660a4076e86f
A_J <Ahmed.j.jibril@users.noreply.github.com>
2020-09-24 15:50:21 +0200

    Just learning

---
+++ index.js
@@ -0,0 +1,56 @@
+class calculator {
+  constructor(previousoperandtextelement, currentoperandtextelement) {
+    this.previousoperandtextelement = previousoperandtextelement;
+    this.currentoperandtextelement = currentoperandtextelement;
+    this.clear()
+  }
+}
+
+clear() {
+this.currentoperand = ''
+this.previousoperand = ''
+this.operation = undefined
+}
+
+delete() {
+
+}
+
+appendnumber(number) {
+this.currentoperand = number
+}
+
+chooseOperation(operation) {
+
+}
+
+compute() {
+
+}
+
+updatedisplay() {
+    this.currentoperandtextelement.innertext = this.currentoperand
+}
+
+
+const numberbuttons = document.querySelectorAll("[data-number]");
+const operationbutton = document.querySelectorAll("[data-operation]");
+const equalsbutton = document.querySelectorAll("[data-equals]");
+const deletebutton = document.querySelectorAll("[data-delete]");
+const allclearbutton = document.querySelectorAll("[data-all-clear]");
+const previousoperandtextelement = document.querySelectorAll(
+  "[data-previous-operand]"
+);
+const currentoperandtextelement = document.querySelectorAll(
+  "[data-current-operand]"
+);
+
+
+const calculator = new Calculator(previousoperandtextelement, currentoperandtextelement)
+
+numberbuttons.forEach(buttons => [
+    button.addeventlistener('click', () => {
+        Calculator.appendnumber(button.innertext)
+        Calculator.updatedisplay()
+    })
+]) 
